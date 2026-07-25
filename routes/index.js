8b0abcfa8f1d762347ac8a97d5a62a004f8d0d7f
DrexSabino <sabino.dxt27@gmail.com>
2026-07-25 14:38:11 -0400

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { 
+    title: 'Home' 
+  });
+});
+
+router.get('/plan-trip', function(req, res) {
+  res.render('plan-trip', { 
+    title: 'Plan a Trip' 
+  });
+});
+
+router.get('/saved-trips', function(req, res) {
+  res.render('saved-trips', { 
+    title: 'Saved Trips' 
+  });
+});
+
+router.get('/profile', function(req, res) {
+  res.render('profile', { 
+    title: 'Profile' 
+  });
+});
+
+module.exports = router;
